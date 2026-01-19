6f9efa15f4b6048e14fc4db2ff70c05254ce5746
TranslucentFoxHuman <tlfoxhuman@example.com>
2026-01-19 20:21:14 +0900

    saved unsaved file

--- win11.ps1
+++ win11.ps1
@@ -1,1 +1,5 @@
- 
+﻿# Windows 11 Easy Setup Script downloader
+
+Invoke-WebRequest -Uri "https://github.com/TranslucentFoxHuman/System-Easy-Setup-Script/raw/refs/heads/main/11Tweaks.ps1" -OutFile "$env:TEMP\11Tweaks.ps1"
+
+Start-Process powershell -executionpolicy bypass -Verb runAs "$env:TEMP\11Tweaks.ps1"
